f8a98c19e25728ff4bddc7533318eb6c01d8ee7b
nozomi46 <nozomi46.nk@gmail.com>
2020-07-25 14:08:22 +0900

    現段階でやった分です。

--- src/html/recipe/add.php
+++ src/html/recipe/add.php
@@ -20,8 +20,8 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+            <ul class="navbar-nav mr-auto">＾：
+                １ <li class="nav-item active">
                     <a class="nav-link" href="./">レシピ一覧 <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
@@ -146,8 +146,9 @@
     <!-- コンテナ ここまで -->
 
     <!-- 必要なJavascriptを読み込む -->
-    <script src="../js/jquery-3.4.1.min.js"></script>
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </body>
 
--- src/html/recipe/delete.php
+++ src/html/recipe/delete.php
@@ -62,7 +62,7 @@
     <div class="container">
         <h1>レシピ削除</h1>
         <form action="./action.php" method="POST" enctype="multipart/form-data">
-        こちらのレシピを削除しますか？
+            こちらのレシピを削除しますか？
         </form>
     </div>
     <input type="submit" value="削除" class="btn btn-primary">
@@ -72,8 +72,9 @@
     <!-- コンテナ ここまで -->
 
     <!-- 必要なJavascriptを読み込む -->
-    <script src="../js/jquery-3.4.1.min.js"></script>
-    <script src="../js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </body>
 
